498c1f5501b12b7c62597dd3a5406ec84134a134
Daniel Yepie <yepiengesso@gmail.com>
2023-12-13 04:21:13 +0100

    premier commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, {FunctionComponent} from "react";
+
+const App: FunctionComponent=()=>{
+
+
+    return(
+        <div>
+            <p>bienvenue</p>
+        </div>
+    )
+}
+
+export default App;
